315355eb47994bf03cb46322b6a6f28e9e13f251
Balaram Sahu <39689150+balia4u@users.noreply.github.com>
2018-09-16 17:37:56 +0530

    Host and Port parameterized.

--- src/main/java/com/bigbasket/GRPCClient/client/Communication_service.java
+++ src/main/java/com/bigbasket/GRPCClient/client/Communication_service.java
@@ -27,6 +27,7 @@ import io.grpc.examples.helloworld.CommunicationResponse;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 import com.google.gson.JsonIOException;
 import com.google.gson.JsonParser;
@@ -70,14 +71,11 @@ public class Communication_service {
 			try {
 				request = CommunicationRequest.newBuilder()
 						.setBbsign("99f1e07b5a5adde0311790bfdb6d41d4807163ee006af3aecaa08f6d4a93e392:bWVtYmVyX2lk")
-						//.setBbsign("bbcc80c9bc902a7018f14f45035eafa160a000dd28fae34d9c70a40152b14328:bWVtYmVyX2lk")
-						.setChannel("unified")
-						// .setCommunicationType("rating_message")
-						.setCommunicationType("order_place").setDataMap(getJSON_Body()).build();
+						.setChannel("unified").setCommunicationType("order_place").setDataMap(getJSON_Body()).build();
 				response = stub.communicationRequestHandler(request);
-				// logger.info("\n Request -> " + request + "\n Response -> " + response);
+				logger.info("\n Request -> " + request + "\n Response -> " + response);
 			} catch (StatusRuntimeException e) {
-				// logger.log(Level.WARNING, "RPC failed: {0}", e.getStatus());
+				logger.log(Level.WARNING, "RPC failed: {0}", e.getStatus());
 				e.getMessage();
 				return;
 
@@ -87,17 +85,12 @@ public class Communication_service {
 	}
 
 	private String getJSON_Body() {
-		/*
-		 * JsonObject createIssue = Json.createObjectBuilder().add("email",
-		 * "balaram.sahu1@bigbasket.com") .add("mobile_number",
-		 * "1111111111").add("support_phone", "8000089900") .add("member_first_name",
-		 * "Midhun").add("delivery_time", "2017-8-12 17:54:00").add("order_id", 1234)
-		 * .add("link", "http://example.com").add("member_id",
-		 * 4433470).add("cookie_value", "MzE0NTk2ODMzNg==") .build();
-		 */
 		JsonParser parser = new JsonParser();
 		try {
-			return parser.parse(new JsonReader(new FileReader(System.getProperty("user.dir")+"//src//main//resources//order.json"))).toString();
+			return parser
+					.parse(new JsonReader(
+							new FileReader(System.getProperty("user.dir") + "//src//main//resources//order.json")))
+					.toString();
 		} catch (JsonIOException | JsonSyntaxException | FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			System.out.println("Problem in reading Data map");
@@ -107,16 +100,23 @@ public class Communication_service {
 	}
 
 	public static void main(String[] args) {
-		//Communication_service cs = new Communication_service("communication-qas.bigbasket.com", 6667);
-		Communication_service cs = new Communication_service("internal-aedd54186b7f111e8a122129d77c7ac6-1636873293.us-east-1.elb.amazonaws.com", 6667);
-		cs.CommunicationCall();
-		System.out.println(cs.response);
-		String res = "" + cs.response;
-		if (res.contains("queued"))
-			getResult = true;
-		else
-			getResult = false;
-		shutdown();
+		try {
+			String host = System.getProperty("host");
+			int port = Integer.parseInt((System.getProperty("port")).replaceAll("[\\D]", ""));
+			Communication_service cs = new Communication_service(host, port);
+			cs.CommunicationCall();
+			System.out.println(cs.response);
+			String res = "" + cs.response;
+			if (res.contains("queued"))
+				getResult = true;
+			else
+				getResult = false;
+		}
+			catch(Exception e) {
+				logger.log(Level.SEVERE, "RPC failed due to some error");
+			}
+		finally {
+			shutdown();
+		}
 	}
-
 }
